572fd52f6547b92742eef33eb1ac15e13d5f1d0a
James Córdoba Jr <cordoba.james81@gmail.com>
2022-08-23 20:54:45 -0500

    Nueva clase - Empresa

--- src/main/java/com/solvers/proyectoempresa/Empresa.java
+++ src/main/java/com/solvers/proyectoempresa/Empresa.java
@@ -2,4 +2,60 @@ package com.solvers.proyectoempresa;
 
 public class Empresa {
 
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private String nit;
+
+
+    //Constructor (permite crear una instancia de la clase "Empresa")
+    public Empresa(String nombre, String direccion, int telefono, String nit) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
+
+    public Empresa() {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
+
+    //Getters & Setters (permiten leer y modificar los atributos de la empresa creada)
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
 }
+
