b31c9caaa87256866014ba301fbd84dcaf13e0d3
jenvasquez <jenvasquezh@gmail.com>
2022-09-07 09:28:16 -0500

    Se crean controladores, entidades y servicios de MovimientoDinero

---
+++ src/main/java/com/solvers/proyectoempresa/controllers/MovimientosControllers.java
@@ -0,0 +1,52 @@
+package com.solvers.proyectoempresa.controllers;
+
+import com.solvers.proyectoempresa.entities.MovimientoDinero;
+import com.solvers.proyectoempresa.service.MovimientoService;
+import com.solvers.proyectoempresa.service.Response;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("movimientos")
+public class MovimientosControllers {
+    private MovimientoService movimientoService;
+    public MovimientosControllers(MovimientoService service){
+        this.movimientoService=service;
+
+    }
+    @RequestMapping("/movimiento")
+    public String movimientos(){
+        return "controlador movimiento";
+    }
+
+    @RequestMapping("getmovimientosdinero")
+    public ArrayList<MovimientoDinero> selectAll(){
+        return this.movimientoService.selectAll();
+    }
+
+    @RequestMapping("getmovimientodinero/{id}")
+    public MovimientoDinero selectId(@PathVariable int id){
+        return this.movimientoService.selectByID(id);
+    }
+
+    @PostMapping("create")
+    public Response createMovimiento(@RequestBody MovimientoDinero request){
+        return this.movimientoService.createMovimiento(request);
+
+
+    }
+
+    @DeleteMapping("delete/{id}")
+    public Response deleteMovimiento(@PathVariable int id){
+        return this.movimientoService.deleteMovimientoById(id);
+    }
+
+    @PutMapping("update")
+    public Response updateMovimiento(@RequestBody MovimientoDinero request){
+        return this.movimientoService.updateMovimientoById(request);
+    }
+
+
+
+}
---
+++ src/main/java/com/solvers/proyectoempresa/entities/MovimientoDinero.java
@@ -0,0 +1,61 @@
+package com.solvers.proyectoempresa.entities;
+
+import com.solvers.proyectoempresa.Empleado;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "movimientodinero")
+public class MovimientoDinero {
+
+    @Column(name = "monto")
+    private float monto;
+
+    @Column(name = "conceptomovimiento")
+    private String conceptoMovimiento;
+
+    /*@Column(name = "empleado")
+    private Empleado empleado;
+    */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMovimiento;
+
+
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+   /* public Empleado getEmpleado() {
+        return empleado;
+    }
+    */
+
+    public int getIdMovimiento() {
+        return idMovimiento;
+    }
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+   /* public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+    */
+
+
+    public void setIdMovimiento(int idMovimiento) {
+        this.idMovimiento = idMovimiento;
+    }
+
+}
